057f1b89713c76917e664327223a1c6b41041985
kfotkin <68823654+kfotkin@users.noreply.github.com>
2022-02-09 21:49:04 +0300

    upd_09_02

--- my_shop/mainapp/urls.py
+++ my_shop/mainapp/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< Updated upstream
     path("", views.index, name="main"),
     path("contact", views.contact, name="contact"),
     path("products", views.products, name="products"),
     path("products/<int:pk>", views.category, name="category"),
+=======
+    path("", views.products, name="all"),
+    path("<int:category_id>", views.category, name="category"),
+    path("product/<int:product_id>", views.product, name="product"),
+>>>>>>> Stashed changes
 ]
--- my_shop/mainapp/views.py
+++ my_shop/mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.request import HttpRequest
 from .models import Product
 from .models import ProductCategory
+import random
 
 MENU_LINKS = [
     {"url": "main", "name": "домой"},
@@ -20,6 +21,22 @@ def index(request):
     )
 
 
+def product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    categories = ProductCategory.objects.all()
+    return render(
+        request,
+        "mainapp/product.html",
+        context={
+            "title": "Продукты",
+            "menu_links": MENU_LINKS,
+            "product": product,
+            "categories": categories,
+            "category": product.category,
+        },
+    )    
+
+
 def contact(request):
     return render(
         request,
@@ -31,8 +48,13 @@ def contact(request):
     )
 
 
+def get_hot_product(queryset):
+    return random.choice(queryset)
+
+
 def products(request):
     categories = ProductCategory.objects.all()
+<<<<<<< Updated upstream
     products = [
         {
             "name": "стул",
@@ -45,6 +67,31 @@ def products(request):
             "image": "mainapp/img/product-21.jpg",
         },
     ]
+=======
+    products = Product.objects.all()
+    hot_product = get_hot_product(products)
+>>>>>>> Stashed changes
+    return render(
+        request,
+        "mainapp/products.html",
+        context={
+            "title": "Продукты",
+            "menu_links": MENU_LINKS,
+            "products": products.exclude(pk=hot_product.pk),
+            "categories": categories,
+            "hot_product": hot_product,
+        },
+    )
+
+
+<<<<<<< Updated upstream
+def category(request, pk):
+    return products(request)
+=======
+def category(request, category_id):
+    categories = ProductCategory.objects.all()
+    category = get_object_or_404(ProductCategory, pk=category_id)
+    products = Product.objects.filter(category=category)
     return render(
         request,
         "mainapp/products.html",
@@ -53,9 +100,8 @@ def products(request):
             "menu_links": MENU_LINKS,
             "products": products,
             "categories": categories,
+            "hot_product": get_hot_product(products),
+            "category": category,
         },
     )
-
-
-def category(request, pk):
-    return products(request)
+>>>>>>> Stashed changes
