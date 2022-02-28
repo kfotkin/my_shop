5a85997a5f7911eb104835723d40512266f36032
kfotkin <68823654+kfotkin@users.noreply.github.com>
2022-02-28 21:57:30 +0300

    upd_28_02

--- my_shop/ordersapp/models.py
+++ my_shop/ordersapp/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product
+from basketapp.models import Basket
 
 # Create your models here.
 
 
 class Order(models.Model):
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
+        ordering = ('-created',)
+    
     CREATED = 'CREATED'
     IN_PROCESSING = 'IN_PROCESSING'
     AWAITING_PAYMENT = 'WAITING_PAYMENT'
@@ -27,21 +34,31 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created = models.DateTimeField(verbose_name='создан', auto_now_add=True)
     updated = models.DateTimeField(verbose_name='обновлен', auto_now=True)
-    status = models.CharField(verbose_name="Статус", choices=ORDER_STATUS_CHOICES, max_length=20)
+    status = models.CharField(verbose_name="Статус", choices=ORDER_STATUS_CHOICES, max_length=20, default=CREATED)
     is_active = models.BooleanField(verbose_name='активен', default=True)
+
+    @property
+    def items_with_products(self):
+        return self.items.select_related('product')
 
     def __str__(self):
         return f'Заказ {self.id}'
 
-    def get_total_cost(self):
-        return sum(item.cost for item in self.items.select_related('product'))
+    def get_total_cost(self): 
+        return sum(item.cost for item in self.items_with_products)
 
+    def get_total_quantity(self):
+        return sum(item.quantity for item in self.items_with_products)
 
-    class Meta:
-        verbose_name = 'заказ'
-        verbose_name_plural = 'заказы'
-        ordering = ('-created',)
-
+    def delete(self):
+        for item in self.items_with_products:
+            Basket.objects.create(
+                user=self.user, 
+                product=item.product, 
+                quantity=item.quantity
+                )
+        self.is_active = False
+        self.save()
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
--- my_shop/ordersapp/views.py
+++ my_shop/ordersapp/views.py
@@ -18,64 +18,84 @@ class OrderDetail(DetailView):
     model = Order
 
 
-class OrderCreate(CreateView):
+class OrderEditMixin:
+    def make_formset(self, instance=None):
+        OrderFormSet = inlineformset_factory(
+        Order, 
+        OrderItem, 
+        form=OrderItemForm, 
+        extra=1
+        )
+        formset = OrderFormSet(instance=instance)
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST, instance=instance)
+        else:
+            if not instance:
+                basket_items = self.request.user.basket.all()
+                if len(basket_items):
+                    OrderFormSet = inlineformset_factory(
+                        Order, 
+                        OrderItem, 
+                        form=OrderItemForm, 
+                        extra=len(basket_items)
+                        )
+                    formset = OrderFormSet()
+                    for num, form in enumerate(formset.forms):
+                        form.initial['product'] = basket_items[num].product
+                        form.initial['quantity'] = basket_items[num].quantity
+                    basket_items.delete()
+
+        return formset
+
+    def save_formset(self, form, formset, instance=None):
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if formset.is_valid():
+                formset.instance = self.object
+                formset.save()
+        if not self.object.get_total_cost():
+            self.object.delete()
+
+
+class OrderCreate(OrderEditMixin, CreateView):
     model = Order
     form_class = OrderForm
     success_url = reverse_lazy('ordersapp:orders_list')
 
     def get_context_data(self, **kwargs):
         data = super(OrderCreate, self).get_context_data(**kwargs)
-        OrderFormSet = inlineformset_factory(
-            Order, 
-            OrderItem, 
-            form=OrderItemForm, 
-            extra=1
-            )
-        if self.request.POST:
-            formset = OrderFormSet(self.request.POST)
-        else:
-            basket_items = self.request.user.basket.all()
-            if len(basket_items):
-                OrderFormSet = inlineformset_factory(
-                    Order, 
-                    OrderItem, 
-                    form=OrderItemForm, 
-                    extra=len(basket_items)
-                    )
-                formset = OrderFormSet()
-                for num, form in enumerate(formset.forms):
-                    form.initial['product'] = basket_items[num].product
-                    form.initial['quantity'] = basket_items[num].quantity
-                basket_items.delete()
-            else:
-                formset = OrderFormSet()
-
-        data['orderitems'] = formset
+        data['orderitems'] = self.make_formset()
         return data
 
     def form_valid(self, form):
         context = self.get_context_data()
-        orderitems = context['orderitems']
-
-        with transaction.atomic():
-            form.instance.user = self.request.user
-            self.object = form.save()
-            if orderitems.is_valid():
-                orderitems.instance = self.object
-                orderitems.save()
-        if not self.object.get_total_cost():
-            self.object.delete()
+        orderitems = context['orderitems']     
+        self.save_formset(form, orderitems) 
         return super(OrderCreate, self).form_valid(form)
 
 
 
-class OrderUpdate(UpdateView):
+class OrderUpdate(OrderEditMixin, UpdateView):
     model = Order
+    form_class = OrderForm
     success_url = reverse_lazy('ordersapp:orders_list')
+
+    def get_context_data(self, **kwargs):
+        data = super(OrderUpdate, self).get_context_data(**kwargs)
+        data['orderitems'] = self.make_formset(instance=self.object)
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']     
+        self.save_formset(form, orderitems, instance=self.object) 
+        return super(OrderUpdate, self).form_valid(form)
 
 
 class OrderDelete(DeleteView):
     model = Order
+    success_url = reverse_lazy('ordersapp:orders_list')  
 
 
 def order_forming_complete(request):
